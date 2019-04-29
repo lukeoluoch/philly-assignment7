46640216c7ec405aab5a751ef591f7e4ec412f69
Mingyuan Shan <schindlermac@gmail.com>
2019-04-29 01:17:26 -0400

    v 0.3

--- survey.php
+++ survey.php
@@ -26,20 +26,19 @@
     Reason:<br>
     <input type="text" name="reason" value="">
     <br>
-</form>
-<h2>Question 2</h2>
-<p>What is the most important factor when you are deciding which park to visit?</p>
-<form>
+
+    <h2>Question 2</h2>
+    <p>What is the most important factor when you are deciding which park to visit?</p>
+
     <input type="radio" name="factor" value="distance"> Distance
     <input type="radio" name="factor" value="natural views"> Natural Views
     <input type="radio" name="factor" value="wildlife"> Wildlife
     <input type="radio" name="factor" value="culture"> Culture
     <input type="radio" name="factor" value="weather"> Weather
-</form>
-<h2>Question 3</h2>
-<p>Do you think preserving national parks are important for human beings? And why?</p>
 
-<form>
+    <h2>Question 3</h2>
+    <p>Do you think preserving national parks are important for human beings? And why?</p>
+
     <select>
         <option value="yes">Yes</option>
         <option value="no">No</option>
@@ -48,6 +47,15 @@
     Reason:<br>
     <input type="text" name="reason" value="">
     <br><br>
+
+    <h2>Personal information</h2>
+    <p>Our supervisor demanded that you leave your personal contact information</p>
+    Full Name:<br>
+    <input type="text" name="name" value=""><br>
+    Email:<br>
+    <input type="text" name="email" value=""><br>
+    Location:<br>
+    <input type="text" name="location" value=""><br>
     <input type="submit" value="Submit">
 </form>
 <p>This survey is used for research purpose only. All private information would be protected.</p>
