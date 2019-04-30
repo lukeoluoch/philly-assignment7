04e51ecf711c027e1d78a3f97022426fa3e723f9
Thomas Hogrefe <tomhogrefe46@gmail.com>
2019-04-30 13:31:45 -0400

    first round of design updates [WIP]

--- thank-you.php
+++ thank-you.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta lang="en">
-    <title>Thank You</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-
-</head>
-<body class="container">
-<h1>Thank you</h1>
-<nav class="navbar  navbar-expand-lg navbar-light bg-light">
-    <ul class="navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="descriptions.php">Description</a> </li>
-        <li class="nav-item"><a class="nav-link" href="national-park.php">National Parks</a> </li>
-        <li class="nav-item"><a class="nav-link" href="survey.php">Survey</a> </li>
-    </ul>
-</nav>
+<?php include 'inc/html-top.inc'; ?>
+<h2>Thank you!</h2>
 <p>Thank you for completing the survey. We value your contribution and appreciate your time and effort.</p>
 
 <?php
